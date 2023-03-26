3aa2e09915c451f2fdfb18632f23299b9e90e4d0
marouane <uanemaro216@gmail.com>
2023-03-26 17:18:13 +0000

    UPDATE

--- resources/views/galery.blade.php
+++ resources/views/galery.blade.php
@@ -1,16 +1,4 @@
 <x-app-layout>
-    <section class="bg-white dark:bg-gray-900">
-        <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
-            <div class="mr-auto place-self-center lg:col-span-7">
-                <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">Welcome to Literary Hub</h1>
-                <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">an online community for book lovers.</p>
-                <a href="/groups" class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
-                    Get started
-                    <svg class="w-5 h-5 ml-2 -mr-1" fill="white" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                </a>
-            </div>
-        </div>
-    </section>
 
   <div class="py-5 flex justify-center flex-wrap">
     <div class="pt-5 w-full flex justify-center">
@@ -31,10 +19,6 @@
         <x-input-label for="Book_Title" :value="__('Book Title')" />
         <x-text-input id="Book_Title" name="Book_Title" type="text" class="mt-1 block w-full" />
       </div>
-      <div class="mr-3">
-        <x-input-label for="date" :value="__('The Date')" />
-        <x-text-input id="date" name="date" type="date" class="mt-1 block w-full" />
-      </div>
       <div class="w-full">
         <x-primary-button type="submit" class="h-10">{{ __('Submit') }}</x-primary-button>
       </div>
@@ -52,7 +36,7 @@
         src="{{ $book->book_cover }}"
         alt="" />
       <div style="height: 400px" class="flex flex-col justify-start p-6 overflow-y-auto">
-        <div class="w-20 flex flex-col items-baseline">
+        <div class="w-20 flex flex-col items-baseline" style="width: auto;">
           <div>
         @if (isset($book->favourites[0]))
           @foreach($book->favourites as $key => $user)
--- resources/views/groups/view.blade.php
+++ resources/views/groups/view.blade.php
@@ -1,16 +1,5 @@
 <x-app-layout>
-      <section class="bg-white dark:bg-gray-900">
-        <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
-            <div class="mr-auto place-self-center lg:col-span-7">
-                <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">Welcome to Literary Hub</h1>
-                <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">an online community for book lovers.</p>
-                <a href="#" class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
-                    Get started
-                    <svg class="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                </a>
-            </div>
-        </div>
-    </section>
+
             @if ($errors->any())
               <div id="alert-2" class="flex p-4 mb-4 text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
                 <svg aria-hidden="true" class="flex-shrink-0 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path></svg>
