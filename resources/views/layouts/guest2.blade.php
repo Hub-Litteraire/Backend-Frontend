a59d30b4ec5be602b4d144c3925036318b54f0a0
marouane <uanemaro216@gmail.com>
2023-03-27 14:31:37 +0000

    update

--- resources/views/layouts/guest2.blade.php
+++ resources/views/layouts/guest2.blade.php
@@ -27,16 +27,7 @@
         </main>
 
         <footer class="p-4 bg-white rounded-lg shadow md:px-6 md:py-8 dark:bg-gray-900">
-            <div class="sm:flex sm:items-center sm:justify-between">
-                <a href="https://flowbite.com/" class="flex items-center mb-4 sm:mb-0">
-                    <x-application-logo></x-application-logo>
-                </a>
-            </div>
-            <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-            <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="#"
-                    class="hover:underline">
-                    <x-application-logo></x-application-logo>
-                    </a>. All Rights Reserved.
+            <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="#" class="hover:underline">HUB LITTÈRAIRE</a>. All Rights Reserved.
             </span>
         </footer>
     </div>
